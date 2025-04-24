9fb6ddff94ff31bc3c88961240e1f084518dc7e1
iQueryData <157438328+iQueryData@users.noreply.github.com>
2025-04-24 05:46:15 +0000

    multiple join -every admission

---
+++ Medium/Joins/doctor_patient_fullname_diagnosis_every_admission.sql
@@ -0,0 +1,23 @@
+
+
+/*
+For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem
+
+
+*/
+
+Solution:
+
+select concat(p.first_name, " ", p.last_name) as patient_name, a.diagnosis, 
+concat(d.first_name, " ", d.last_name) as doctor_name
+from 
+patients p 
+Left JOIN 
+admissions a 
+on p.patient_id = a.patient_id
+left join 
+doctors d 
+ON 
+a.attending_doctor_id = d.doctor_id
+where diagnosis is not NULL
+
